f8ab20c879d42f07895fe59c7b7efd496d05ee0f
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-06-16 15:41:55 -0400

    .

--- back-end.js
+++ back-end.js
@@ -4,6 +4,14 @@ function whatCanIDoLegally(age){
       return " You can't do anything."
   }
   //if it's between 18 and 20 inclusive, they can drive
-
+  else if(age >= 18 && age <= 20){
+return "You can drive but don't drink."
+  }
+  else if (age >= 21){
+      return "You can do either but don't do both at the same time"
+  }
   // if it's at least 21, they can do both
 }
+
+module.exports=whatCanIDoLegally;
+// exists within node
