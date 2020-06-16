9f5bc4a0eba1ea68db339dc4c21bbd0c04f20496
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-06-16 14:52:36 -0400

    under 18

---
+++ back-end.js
@@ -0,0 +1,9 @@
+function whatCanIDoLegally(age){
+  //if it's under 18, they can't drive or drink
+  if( age < 18){
+      return " You can't do anything."
+  }
+  //if it's between 18 and 20 inclusive, they can drive
+
+  // if it's at least 21, they can do both
+}
