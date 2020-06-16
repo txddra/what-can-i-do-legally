c5207c4b6c7631f26e882b6903d17bedf79acee8
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-06-16 15:42:12 -0400

    .

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
+const whatCanIDoLegally = require('./back-end.js')
+//path to the file
 
+//gets users age from the console
+const age = getInput(0);
 
+//get a message based on that age from the back end
+const message = whatCanIDoLegally(age);
 
+// print it out for the user
 
+console.log(message)
 function getInput(i) {
   return process.argv[i + 2];
 }
